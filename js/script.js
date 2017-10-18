0ae6b3e531e408fbc42699044710fdf8b4fc69e9
geroas <alex@MacBook-Pro-de-Alexandre.local>
2017-10-18 22:10:19 +0200

    La carte affiche désormais la position de l’internaute

--- js/script.js
+++ js/script.js
@@ -10,6 +10,49 @@ function geoFindMe() {
     function success(position) {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
+        
+        window.globalLat = position.coords.latitude;;
+        window.globalLng = position.coords.longitude;
+        
+        
+    
+
+    
+     var locations = [
+      ['Bondi Beach', latitude, longitude, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) { 
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+    
+
+        
+        
 
     output.innerHTML = '<p>Votre latitude : ' + latitude + '° <br>Votre longitude : ' + longitude + '°</p>';
 
@@ -98,5 +141,9 @@ getJSON('index.php?latitude='+ current_position.latitude + '&longitude='+ curren
 
     navigator.geolocation.getCurrentPosition(success, error);
 }
+
+
+
+
 
 geoFindMe();
